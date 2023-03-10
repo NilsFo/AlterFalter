b9a613879ab7f5bb89dc2811713403c179db3169
NilsFo <nils.foerster@yahoo.de>
2023-03-10 21:49:58 +0100

    Made many changes, playable scene is one of them

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player Movement")] public bool movementEnabled = true;
+    public PlayerMovementParam movementParams;
+
+    private Rigidbody2D myRigidbody;
+    private Collider2D myCollider;
+
+    public float movementForce;
+
+    private void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //myRigidbody.angularDrag = movementParams.angularDrag;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector2 force = new Vector2(movementForce, 0);
+            myRigidbody.AddForce(force);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector2 force = new Vector2(-movementForce, 0);
+            myRigidbody.AddForce(force);
+        }
+    }
+}
---
+++ Assets/Scripts/data/PlayerMovementParam.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/PlayerMovementParam", order = 1)]
+public class PlayerMovementParam : ScriptableObject
+{
+    public bool canJump=false;
+    public float velocity = 0;
+    public float angularDrag;
+}
