5d583cc5ab49ac6d2d29ae6c3fced32624201a13
NilsFo <nils.foerster@yahoo.de>
2023-03-16 16:30:56 +0100

    Updated levels, merge stuff, updated UI

--- Assets/Scripts/movement/Better_Worm_Movement.cs
+++ Assets/Scripts/movement/Better_Worm_Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -224,9 +225,7 @@ void MoveWormEndToWorm()
 
 
 
-
-
-    bool IsWithinPerimeter(Vector3 targetPosition)
+bool IsWithinPerimeter(Vector3 targetPosition)
     {
         float distance = Vector3.Distance(targetPosition, wormEnd.transform.position);
         return distance <= maxDistanceFromWormEnd;
