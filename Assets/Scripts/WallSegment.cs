c487b57e15e96acd27beba45a897ef5a4d3f9f24
NilsFo <nils.foerster@yahoo.de>
2023-03-17 14:16:51 +0100

    Finished level 4. Implemented breakable walls.

--- Assets/Scripts/WallSegment.cs
+++ Assets/Scripts/WallSegment.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class WallSegment : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public TimedLife myLife;
 
     private void Start()
     {
@@ -15,5 +16,16 @@ public class WallSegment : MonoBehaviour
     public void Release(float velocity)
     {
         rb.constraints = RigidbodyConstraints2D.None;
+
+        var pos = transform.position;
+        pos.z = -9;
+        transform.position = pos;
+
+        Vector2 vel = new Vector2();
+        vel.x = velocity * 10f;
+        vel.y = Mathf.Abs(velocity) * 5f;
+
+        rb.AddForce(vel);
+        myLife.timerActive = true;
     }
 }
